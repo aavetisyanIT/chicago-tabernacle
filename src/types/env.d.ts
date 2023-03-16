9d7a208c472a60a17b0906f66fc821071cd9b270
aavetisyanit <aavetisyanit@gmail.com>
2023-03-16 17:10:25 -0500

    Add more dependencies and .env

---
+++ src/types/env.d.ts
@@ -0,0 +1,10 @@
+declare module '@env' {
+  export const DEV_WEBCLIENTID: string;
+  export const DEV_APIKEY: string;
+  export const DEV_AUTHDOMAIN: string;
+  export const DEV_PROJECTID: string;
+  export const DEV_STORAGEBUCKET: string;
+  export const DEV_MESAGINGSENDERID: string;
+  export const DEV_APPID: string;
+  export const DEV_MEASUREMENTID: string;
+}

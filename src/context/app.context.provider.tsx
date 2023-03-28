b6642eb66061e44dd44f835d3223eb668ca74ba0
aavetisyanit <aavetisyanit@gmail.com>
2023-03-28 16:16:36 -0500

    refactor App Context types

--- src/context/app.context.provider.tsx
+++ src/context/app.context.provider.tsx
@@ -4,11 +4,9 @@ import {AppContext} from './app.context';
 import reducer from './app.context.reducer';
 import initialState from './initialState';
 
-interface IAppContextProviderProps {
+export const AppContextProvider: FC<{
   children: ReactNode;
-}
-
-export const AppContextProvider: FC<IAppContextProviderProps> = props => {
+}> = props => {
   const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <AppContext.Provider value={[state, dispatch]}>
--- src/context/app.context.ts
+++ src/context/app.context.ts
@@ -1,6 +1,8 @@
 import {DispatchWithoutAction, createContext} from 'react';
 import initialState, {IInitialState} from './initialState';
 
-export const AppContext = createContext<[IInitialState, DispatchWithoutAction]>(
-  [initialState, () => null],
-);
+//TODO: NEED TO TYPE REDUCER FUNCTION
+
+export const AppContext = createContext<
+  [IInitialState, (param: {type: any; payload: any}) => void]
+>([initialState, () => null]);

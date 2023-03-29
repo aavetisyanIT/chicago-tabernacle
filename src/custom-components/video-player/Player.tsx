6b98c68273ac396d139926cea921d77e9e6417a3
aavetisyanit <aavetisyanit@gmail.com>
2023-03-29 16:51:39 -0500

    Remove issue with full screen icon when player opens

--- src/custom-components/video-player/Player.tsx
+++ src/custom-components/video-player/Player.tsx
@@ -1,27 +1,81 @@
-import React, {FC, useState, useCallback} from 'react';
+import React, {FC, useContext, useCallback} from 'react';
 import VideoPlayer from 'react-native-video-controls';
-import Orientation from 'react-native-orientation-locker';
+
+import {AppContext} from '../../context/app.context';
+import {actionTypes} from '../../context/action.types';
 
 export const Player: FC = () => {
-  const [isVideoPaused, setIsVideoPaused] = useState<boolean>(true);
-  const [isPosterShown, setIsPosterShown] = useState<boolean>(true);
+  const [state, dispatch] = useContext(AppContext);
+  const {isVideoPaused, articleVideoUrl, articleImageUrl, isFullScreenVideo} =
+    state;
 
   const handleOnPlay = useCallback(() => {
-    setIsVideoPaused(false);
-    setIsPosterShown(false);
-  }, []);
+    dispatch({
+      type: actionTypes.SET_PAUSE_VIDEO,
+      payload: false,
+    });
+  }, [dispatch]);
+
+  const handleOnEnd = useCallback(() => {
+    dispatch({
+      type: actionTypes.SET_PAUSE_VIDEO,
+      payload: true,
+    });
+  }, [dispatch]);
+
+  const handleOnEnterFullscreen = useCallback(() => {
+    dispatch({
+      type: actionTypes.SET_FULLSCREEN_VIDEO,
+      payload: true,
+    });
+  }, [dispatch]);
+
+  const handleOnExitFullscreen = useCallback(() => {
+    dispatch({
+      type: actionTypes.SET_FULLSCREEN_VIDEO,
+      payload: false,
+    });
+  }, [dispatch]);
+
+  React.useEffect(() => {
+    return () => {
+      dispatch({
+        type: actionTypes.SET_PAUSE_VIDEO,
+        payload: true,
+      });
+    };
+  }, [dispatch]);
 
   return (
     <VideoPlayer
       paused={isVideoPaused}
+      fullscreen={isFullScreenVideo}
+      videoStyle={{
+        flex: 1,
+      }}
+      toggleResizeModeOnFullscreen
       onPlay={handleOnPlay}
-      onEnterFullscreen={() => Orientation.lockToLandscape()}
-      onExitFullscreen={() => Orientation.lockToPortrait()}
-      tapAnywhereToPause={false}
-      audioOnly={isPosterShown}
+      onEnd={handleOnEnd}
+      // onError={() => console.log('error')}
+      onEnterFullscreen={handleOnEnterFullscreen}
+      onExitFullscreen={handleOnExitFullscreen}
+      tapAnywhereToPause
+      controlAnimationTiming={1000}
+      controlTimeout={25000}
+      scrubbing={10}
+      audioOnly={isVideoPaused}
+      //TODO provide correct source when player is ready
       source={{uri: 'https://vjs.zencdn.net/v/oceans.mp4'}}
+      // source={{
+      //   uri:
+      //     articleVideoUrl ||
+      //     'https://player.vimeo.com/external/535955445.m3u8?s=9b15c3f1d9565e47615953db6c46c27b79c686fb',
+      // }}
       posterResizeMode="cover"
-      poster="https://chitab.org/wp-content/uploads/2023/03/03122023-LLG-APP-COVERS-01.png"
+      poster={
+        articleImageUrl ||
+        'https://chitab.org/wp-content/uploads/2020/09/CT-blank-slide.png'
+      }
       disableBack
     />
   );
